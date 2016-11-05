8aa6de1b74fbadcbd424eb9e52a6077501c25e0c
Kayque <kayque-willy@hotmail.com>
2016-11-05 20:00:49 +0000

    Implementação da Model do repasse financeiro.

---
+++ application/models/Repasse_model.php
@@ -0,0 +1,61 @@
+<?php
+class Repasse_model extends CI_Model{
+ 
+  public $codProjeto;
+  public $necessidade;
+  public $data;
+  public $valorParcela;
+  public $status;
+    
+  #Constroi o objeto
+  public function __construct($codProjeto='', $necessidade='',$data='',$valorParcela='',$status=''){
+     if(isset($codProjeto)) $this->codProjeto=$codProjeto;
+     if(isset($necessidade)) $this->necessidade=$necessidade;
+     if(isset($data)) $this->data=$data;
+     if(isset($valorParcela)) $this->valorParcela=$valorParcela;
+     if(isset($status)) $this->status=$status;
+  }
+  
+  #Insere um novo registro no banco
+  public function insert(){
+     //Cria um vetor de valores para atualização
+     $data = []; 
+     if(isset($this->codProjeto)) $data['codProjeto'] = $this->codProjeto;
+     if(isset($this->necessidade)) $data['necessidade'] = $this->necessidade;
+     if(isset($this->data)) $data['data'] = $this->data;
+     if(isset($this->valorParcela)) $data['valorParcela'] = $this->valorParcela;
+     if(isset($this->status)) $data['status'] = $this->status;
+     return $this->db->insert('repasse',$data);
+  }
+  
+  #Remove um objeto de acordo com o nome
+  public function remove () {
+    $data = [];
+    if(isset($this->codProjeto)) $data['codProjeto'] = $this->codProjeto;
+    if(isset($this->necessidade)) $data['necessidade'] = $this->necessidade;
+    return $this->db->delete('repasse',$data);
+  }
+ 
+  #Atualiza o objeto a partir da chave primaria
+  public function update ($codProjeto='',$necessidade='') {
+     //Cria um vetor de valores para atualização
+     $data = [];
+     if(isset($this->data)) $data['data'] = $this->data;
+     if(isset($this->valorParcela)) $data['valorParcela'] = $this->valorParcela;
+     if(isset($this->status)) $data['status'] = $this->status;
+     //Cria um vetor com a chave primária 
+     $where['codProjeto']=$codProjeto;
+     $where['necessidade']=$necessidade;
+     //$this->db->update(nome da tabela,valores de atualização,referência)
+     return $this->db->update('repasse',$data,$where);
+  }
+  
+  #Retorna o objeto
+  public function select($filtro='') {
+   //Adiciona clausula where
+   if(!empty($filtro['codProjeto'])) $this->db->where('codProjeto', $filtro['codProjeto']);
+   if(!empty($filtro['data'])) $this->db->where('data', $filtro['data']);
+   return $this->db->get('repasse');
+  }
+  
+}
